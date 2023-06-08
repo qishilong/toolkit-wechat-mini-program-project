de0c8b1311b961fd58ae4b71c95715f8cfe81f0d
qishilong <663253062@qq.com>
2023-06-08 22:12:58 +0800

    toolkit 项目初始化
    toolkit 项目初始化

---
+++ components/indexItem/indexItem.js
@@ -0,0 +1,41 @@
+// components/indexItem/indexItem.js
+Component({
+    options: {
+        styleIsolation: 'shared',
+    },
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        name: String,
+        iconUrl: String,
+        itemColor: String,
+        pagePath:String,
+    },
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        name: "尝试一下",
+        iconUrl: "https://images-1305186932.cos.ap-beijing.myqcloud.com/images/202306081657534.svg",
+        itemColor: "114, 236, 232",
+        pagePath:"1111"
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        pageSkipFn(){
+            if(this.data.pagePath === ''){
+                wx.showToast({
+                  title: '新功能正在开发中，敬请期待...',
+                  icon: 'none'
+                })
+            }
+            wx.navigateTo({
+              url: this.data.pagePath,
+            })
+        },
+    }
+})
